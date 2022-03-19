bca10db0d84d650e67f32ea77b04eb0edb250739
Ivan77web <98344638+Ivan77web@users.noreply.github.com>
2022-03-19 23:02:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ function res(){
     let i = 0;
 
     setInterval(() => {
+        
         if(word.length == allWord.length){
             return;
         }
+        
         word = word + allWord[i];
         i++;
-
-    
-
+        
         let j = word.length-1;
         if( word[j] != "<" ){
             if(word.length  == allWord.length){
@@ -26,7 +26,6 @@ function res(){
                 text.innerHTML = word + "_";
             }
         } else{ 
-        
         }
 
     }, 100);
@@ -39,78 +38,21 @@ let buttonTwo = document.querySelector(".buttonTwo");
 res();
 
 button.addEventListener("pointerdown", function(event){
-
     text.innerHTML = "";
 })
 
 buttonTwo.addEventListener("pointerdown", function(){
-
     res();
-
 })
 
-
-
-
-
 document.addEventListener("pointerdown", function(event){
-
     if(event.target.className != ("button" || "buttonTwo")) return;
-    
     event.target.style.background = "red";
 })
 
 document.addEventListener("pointerup", function(event){
-
+    
     if(event.target.className != ("button" || "buttonTwo")) return;
     
     event.target.style.background = null;
 })
-
-
-
-
-
-
-// function res(){
-//     let all = [];
-//     let messages = prompt("Сколько сообщений должно быть?");
-//     for(let i = 1; i <= messages; i++ ){
-//         all[i-1] = "-" + prompt(`Введите ${i} сообщение`);
-//     }
-
-//     let allWord = all.join("<br>");
-//     let text = document.querySelector(".text");
-//     let word = '';
-//     let i = 0;
-
-//     setInterval(() => {
-//         if(word.length == allWord.length){
-//             return;
-//         }
-//         word = word + allWord[i];
-//         i++;
-
-    
-
-//         let j = word.length-1;
-//         if( word[j] != "<" ){
-//             if(word.length  == allWord.length){
-//                 text.innerHTML = word;
-//             } else{
-//                 text.innerHTML = word + "_";
-//             }
-//         } else{ 
-        
-//         }
-
-//     }, 100);
-// }
-
-// res();
-
-// let button = document.querySelector(".button");
-
-// button.addEventListener("click", function(){
-//     text.innerHTML = ""
-// })
